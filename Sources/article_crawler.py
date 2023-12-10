8e56c09ff8f7e6d0e83c283b5bbdf19b460e9e38
speciling <speciling@naver.com>
2023-12-10 23:00:14 +0900

    크롤링 코드 수정
    댓글수 관련 코드 삭제, 기사 본문의 탭 제거

--- Sources/article_crawler.py
+++ Sources/article_crawler.py
@@ -15,18 +15,14 @@ def get_soup(link):
 
 
 # 링크가 주어진 기사의 정보를 리스트로 만들어 반환하는 함수
-# [기사제목, 기사링크, 기사본문, 댓글수] 형태로 반환
+# [기사제목, 기사링크, 기사본문] 형태로 반환
 def crawl_article(article_link):
     soup = get_soup(article_link)
 
     title = soup.select_one("#title_area > span").text
-    body_text = soup.select_one("#dic_area").text.strip().replace("\n", " ")
+    body_text = soup.select_one("#dic_area").text.strip().replace("\n", " ").replace('\t', ' ')
 
-    # 자바스크립트로 정보를 가져오는지 html상에는 정보가 표시되지 않음
-    # 셀레니움을 이용해야 할 것으로 보임
-    comments_num = "0"
-
-    return [title, article_link, body_text, comments_num]
+    return [title, article_link, body_text]
 
 
 # 네이버 뉴스의 랭킹뉴스 페이지에서 언론사별 조회수 상위 5개 기사의 정보를 수집하는 함수
@@ -37,10 +33,13 @@ def crawl_rankings(file_name):
     rankingnews_boxes = soup.select(".rankingnews_box")  # 언론사별 랭킹 5위까지의 기사 정보가 담긴 박스들 선택
 
     with open(file_name, 'w', encoding="utf8") as fp:
-        fp.write("언론사명\t기사제목\t기사링크\t기사본문\t댓글수\n")
+        fp.write("언론사명\t기사제목\t기사링크\t기사본문\n")
 
         for box in rankingnews_boxes:
             company_name = box.select_one(".rankingnews_name").text
+            # 영어 기사는 제외하도록 영문 신문은 수집하지 않음
+            if company_name in ("코리아헤럴드", "코리아중앙데일리"):
+                continue
             articles = box.select("li")
 
             for i, article in enumerate(articles):
@@ -55,5 +54,5 @@ def crawl_rankings(file_name):
 
 
 if __name__ == "__main__":
-    file = f"./data{str(datetime.today().date()).replace('-', '')}.tsv"
+    file = f"./crawledData{str(datetime.today().date()).replace('-', '')}.tsv"
     crawl_rankings(file)
